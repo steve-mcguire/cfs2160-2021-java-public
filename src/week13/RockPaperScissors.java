eec9c19ac3b4737a10c702bd599d6a0884e73145
Steve McGuire <s.mcguire@hud.ac.uk>
2022-01-24 18:11:41 +0000

    Week13

--- src/week13/RockPaperScissors.java
+++ src/week13/RockPaperScissors.java
@@ -3,6 +3,7 @@ package week13;
 import java.util.*;
 
 public class RockPaperScissors {
+    //create Maps to store the values of options and results
     private Map<Integer, String> options = new HashMap<>();
     private Map<Integer, String> results = new HashMap<>();
     private Random rand = new Random();
@@ -14,10 +15,12 @@ public class RockPaperScissors {
     //        //3 = scissors
 
     public RockPaperScissors(){
+        // assign mapped valued of options to ints
         options.put(0, "Rock");
         options.put(1, "Paper");
         options.put(2, "Scissors");
 
+        // assign mapped valued of results to ints
         results.put(-2, "Human Wins");
         results.put(-1, "AI Wins");
         results.put(0, "Draw");
@@ -25,6 +28,12 @@ public class RockPaperScissors {
         results.put(2, "Human Wins");
     }
 
+    /**
+     * Conver User string inpout into int if found in the Map
+     * @param userInput
+     * @return
+     * @throws Exception if user input is not valid
+     */
     public int getUserInput(String userInput) throws Exception {
         //check to see if the user input is in the Map
         if(options.containsValue(userInput)){
@@ -40,18 +49,31 @@ public class RockPaperScissors {
         throw new Exception("Input not valid");
     }
 
+    /**
+     * Calculates the difference between aiValue and userValue,
+     * @param u
+     * @param a
+     * @return Value from Map if diff value present
+     * @throws Exception if diff value not found
+     */
     public String getResult(int u, int a) throws Exception {
         //calculate the difference between the scores
         int diff = a - u;
         //check to see if Map contains the key
         if(results.containsKey(diff)){
-            //return value of given key
+            //return value of given key as found in results Map
             return results.get(diff);
         }
         //if key not found
         throw new Exception("Result Not Found");
     }
 
+    /**
+     * Converts the AI int value into a String
+     * @param aiInput
+     * @return
+     * @throws Exception if AI Value not found, should never get to exception
+     */
     public String getAIValue(int aiInput) throws Exception {
         //check if ai key is in Map, should always be as we set the bound of random to 3
         if(options.containsKey(aiInput)){
@@ -61,6 +83,10 @@ public class RockPaperScissors {
         throw new Exception("AI Input Not Recognised");
     }
 
+    /**
+     * Get random AI value from 0, 1 and 2
+     * @return int value o represent AI value
+     */
     public int getAiInput(){
         return rand.nextInt(3);
     }
